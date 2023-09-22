25323f447742f0e3635328e382364363c8c0ee7b
Michelecorrea <136396202+Michelecorrea@users.noreply.github.com>
2023-09-21 22:33:55 -0300

    usando massa de dados

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,8 +1,9 @@
 /// <reference types="cypress" />
 
 describe('Funcionalidade Pagina de produtos', () => {
+    
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos')
 
     });
     it('Deve selecionar um produto da lista', () => {
@@ -16,16 +17,16 @@ describe('Funcionalidade Pagina de produtos', () => {
     });
 
     it ('Deve adicionar um produto ao carrinho',() => {
-        var quantiadade = 3
+        var quantidade = 10
 
         cy.get('[class="product-block grid"]')
            .contains('Ariel Roll Sleeve Sweatshirt').click()
         cy.get('.button-variable-item-M').click()
         cy.get('.button-variable-item-Purple').click()
-        cy.get('.input-text').clear().type (quantiadade)
+        cy.get('.input-text').clear().type (quantidade)
         cy.get('.single_add_to_cart_button').click()
     
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantiadade)
-        cy.get('.woocommerce-message').should ('contain',quantiadade + ' x "Ariel Roll Sleeve Sweatshirt" foram adicionados no seu carrinho .')
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+        cy.get('.woocommerce-message').should ('contain',quantidade + ' x "Ariel Roll Sleeve Sweatshirt" foram adicionados no seu carrinho .')
          });
 });
